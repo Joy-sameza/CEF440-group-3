5a7c6a299cb35445b7ace86408530e9125ee3729
Johnny Poks <johnpokam95@gmail.com>
2024-07-07 11:43:21 +0100

    Started navigation_page

--- Source code/Frontend/cam_we_go/lib/presentation/pages/nav_rail/home/sub/navigation_page.dart
+++ Source code/Frontend/cam_we_go/lib/presentation/pages/nav_rail/home/sub/navigation_page.dart
@@ -1,21 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
+import '../../../../../core/extensions.dart';
+
 class NavigationPage extends StatelessWidget {
   const NavigationPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-
     final Map<String, String> destinations = GoRouterState.of(context).extra as Map<String, String>;
 
-
-
     return Scaffold(
-      body: Center(
-        child: Text(
-          'From: ${destinations["from"]}}\nTo: {{destinations["to"]}',
-          style: const TextStyle(fontSize: 20),
+      body: SafeArea(
+        child: Stack(
+          alignment: Alignment.bottomCenter,
+          children: [
+            const Placeholder(),
+            Container(
+              height: 5.h,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: context.primaryColor,
+                boxShadow: const [
+                  BoxShadow(
+                    offset: Offset(0, -4),
+                    blurRadius: 4,
+                    color: Colors.black26,
+                  ),
+                ],
+                borderRadius: const BorderRadius.vertical(top: Radius.circular(20)),
+              ),
+              child: Column(
+                children: [
+                  
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
